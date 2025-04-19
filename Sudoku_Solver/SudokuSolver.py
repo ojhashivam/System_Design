f3c25369986857470b86d9d2a706733331e1c4b9
Shivam Ojha <138669700+ojhashivam@users.noreply.github.com>
2025-04-19 13:09:40 +0530

    Update SudokuSolver.py

--- Sudoku_Solver/SudokuSolver.py
+++ Sudoku_Solver/SudokuSolver.py
@@ -1,54 +1,60 @@
 class SudokuSolver:
-
     def solver(self , board):
         if self.solve(board):
-            return board
+            return True
         
-        return "Invalid Sequence Entered"
+        return False
     
     def solve(self , board):
         for i in range(9):
             for j in range(9):
-                if board[i][j] == '.':
-                    for num in range(1,10):
+                if board[i][j] == ".":
+                    for num in range(1 , 10 , 1):
                         if self.isSafe(i , j , str(num) , board):
                             board[i][j] = str(num)
-                            if self.solve(board[:]):
-                                return 1
-                            else:
-                                board[i][j] = '.'
+                            if self.solve(board):
+                                return True
+                            
+                            board[i][j] = "."
                     
-                    return 0
+                    return False
         
-        return 1
+        return True
+                            
+        
     
     def isSafe(self , row , col , num , board):
         for i in range(9):
             if board[row][i] == num:
-                return 0
+                return False
             
             if board[i][col] == num:
-                return 0
+                return False
             
             if board[3*(row//3)+(i//3)][3*(col//3)+(i%3)] == num:
-                return 0
+                return False
         
-        return 1
+        return True
 
 if __name__=="__main__":
     print("Enter the board details below\n")
     print("Note: Enter .(dot) for vacant spaces\n")
-    board = ['']*9
+    board = []
     for i in range(9):
         print("Enter row", i+1)
         inp = list(map(str , input().split()))
-        board[i] = inp
+        board.append(inp)
 
+    print(board)
     object = SudokuSolver()
-    print("The Solved Sudoku Looks Like:")
     solved = (object.solver(board))
 
-    for i in range(9):
-        for j in range(9):
-            print(solved[i][j] , end = ' ')
-        print()
+    if solved:
+        print("The Solved Sudoku Looks Like:")
+        for i in range(9):
+            for j in range(9):
+                print(board[i][j] , end = ' ')
+            print()
+    
+    else:
+        print("Invalid Input")
